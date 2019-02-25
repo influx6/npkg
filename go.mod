a96e8623f03239ae9a3063e6567d5c5dad2a6b15
Alexander Ewetumo <trinoxf@gmail.com>
2019-02-25 08:29:14 +0800

    ADD: Added initializer for zipkin based jaeger tracing

--- go.mod
+++ go.mod
@@ -1,12 +1,20 @@
 module github.com/gokit/npkg
 
 require (
+	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dimfeld/httptreemux v5.0.1+incompatible
 	github.com/gokit/history v0.0.0-20190209080252-7693dffa46f8
 	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e
 	github.com/gorilla/mux v1.7.0
+	github.com/opentracing/opentracing-go v1.0.2
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/stretchr/testify v1.3.0
+	github.com/uber-go/atomic v1.3.2 // indirect
+	github.com/uber/jaeger-client-go v2.15.0+incompatible
+	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
+	go.uber.org/atomic v1.3.2 // indirect
 	golang.org/x/crypto v0.0.0-20190219172222-a4c6cb3142f2
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
 	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4
 )
--- ntrace/ntrace.go
+++ ntrace/ntrace.go
@@ -1,8 +1,59 @@
 package ntrace
 
-import "context"
+import (
+	"context"
+	"io"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-client-go/zipkin"
+)
 
 // Trace defines a function
 func Trace(ctx context.Context, traceName string, do func(context.Context, opentracing.Span)) {
 
+}
+
+// InitZipkinTracer inits the jaeger client
+// Service Name in Jaeger Query - edit accordingly to the Kubernetes service name (ask us if you don't know)
+func InitZipkinTracer(srvName string, samplingServerURL, localAgentHost string) (io.Closer, error) {
+	// Standard option for starting Zipkin based Tracing, please include.
+	var jLogger = jaegerlog.StdLogger
+	var zipkinPropagator = zipkin.NewZipkinB3HTTPHeaderPropagator()
+	var injector = jaeger.TracerOptions.Injector(opentracing.HTTPHeaders, zipkinPropagator)
+	var extractor = jaeger.TracerOptions.Extractor(opentracing.HTTPHeaders, zipkinPropagator)
+	var zipkinSharedRPCSpan = jaeger.TracerOptions.ZipkinSharedRPCSpan(true)
+
+	// Create new jaeger reporter and sampler. URL must be fixed.
+	var samplercfg = &jaegercfg.SamplerConfig{
+		Type:              jaeger.SamplerTypeConst,
+		Param:             1,
+		SamplingServerURL: samplingServerURL,
+	}
+
+	var reportercfg = &jaegercfg.ReporterConfig{
+		LogSpans:           true,
+		LocalAgentHostPort: localAgentHost,
+	}
+
+	// Jaeger sampler and reporter
+	var jMetrics = jaeger.NewNullMetrics()
+
+	var sampler, err = samplercfg.NewSampler(srvName, jMetrics)
+	if err != nil {
+		return nil, err
+	}
+
+	var reporter jaeger.Reporter
+	reporter, err = reportercfg.NewReporter(srvName, jMetrics, jLogger)
+	if err != nil {
+		return nil, err
+	}
+
+	var tracer, closer = jaeger.NewTracer(srvName, sampler, reporter, injector, extractor, zipkinSharedRPCSpan)
+	opentracing.SetGlobalTracer(tracer)
+
+	return closer, nil
 }
