2431d8bfbb56b2e2aed9c6c2b2368ea70fcd796a
Alexander Ewetumo <trinoxf@gmail.com>
2019-03-07 16:19:26 +0800

    UPT: Fixed nreflect.DeepCopy

--- nreflect/deepcopy.go
+++ nreflect/deepcopy.go
@@ -1,7 +1,6 @@
 package nreflect
 
 import (
-	"fmt"
 	"reflect"
 	"unsafe"
 
@@ -11,108 +10,167 @@ import (
 
 // DeepCopy copies giving value returning a new distinct value,
 // else an error due to failure.
+//
+// DeepCopy will never attempt to duplicate the following types, as it will simply
+// return the value it received.
+//
+// 1. Channels
+// 2. Interface
+// 3. unsafe.Pointer
+// 4. Func
+//
+// For structs, we use github.com/jinzhu/copier for copying, so all rules in that library
+// applies here also.
 func DeepCopy(elem interface{}) (interface{}, error) {
-	var baseValue = IndirectValue(ValueOf(elem))
+	var err error
+	var refValue reflect.Value
 
-	var baseType reflect.Type
-	var newType reflect.Value
-
-	switch baseValue.Kind() {
-	case reflect.Map:
-		baseType = reflect.MapOf(baseValue.Type(), baseValue.Elem().Type())
-		newType = reflect.MakeMap(baseType)
-	case reflect.Array:
-		baseType = baseValue.Type()
-		newType = reflect.MakeSlice(baseValue.Type(), baseValue.Len(), baseValue.Cap())
-		fmt.Printf("Array: %#v -> %#v =? %q\n", newType, baseValue, newType.Kind())
-	case reflect.Slice:
-		baseType = baseValue.Type()
-		newType = reflect.MakeSlice(baseValue.Type(), baseValue.Len(), baseValue.Cap())
-		fmt.Printf("Slice: %#v -> %#v = %q\n", baseValue, newType, newType.Kind())
+	switch bm := elem.(type) {
+	case reflect.Type:
+		var base = ValueOf(bm)
+		if bm.Kind() == reflect.Ptr {
+			return base, nil
+		}
+		return IndirectValue(base), nil
+	case reflect.Value:
+		refValue, err = deepCopyValue(IndirectValue(bm), bm.Kind() == reflect.Ptr)
 	default:
-		baseType = TypeOf(elem)
-		newType = reflect.New(baseType)
-		fmt.Printf("Def: %#v -> %#v = %q \n", baseValue, newType, baseType)
+		var base = ValueOf(elem)
+		refValue, err = deepCopyValue(IndirectValue(base), base.Kind() == reflect.Ptr)
 	}
 
-	//newType = IndirectValue(newType)
-
-	switch baseValue.Kind() {
-	case reflect.Map:
-		if err := copyMap(newType, baseValue); err != nil {
-			return nil, err
-		}
-	case reflect.Struct:
-		copier.Copy(newType.Interface(), elem)
-	case reflect.UnsafePointer:
-	case reflect.Slice:
-		reflect.Copy(newType, baseValue)
-	case reflect.String:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		fmt.Printf("Value: %#v -> %#v \n", newType, baseValue)
-		newType.Set(baseValue)
-	case reflect.Float64, reflect.Float32:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		newType.Set(baseValue)
-	case reflect.Bool:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		newType.Set(baseValue)
-	case reflect.Chan:
-		return nil, nerror.New("unable to copy channel type")
-	case reflect.Array:
-		reflect.Copy(newType, baseValue)
-	case reflect.Complex64:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		newType.Set(baseValue)
-	case reflect.Complex128:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		newType.Set(baseValue)
-	case reflect.Uintptr:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		newType.Set(baseValue)
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		newType.Set(baseValue)
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		if newType.CanSet() {
-			return nil, nerror.New("unable to set value for type")
-		}
-		newType.Set(baseValue)
+	if err != nil {
+		return nil, err
 	}
-	return newType.Interface(), nil
+	return refValue.Interface(), nil
 }
 
-func copyMap(dest reflect.Value, src reflect.Value) error {
+// DeepCopyValue uses the internal deepCopyValue function to make a copy of giving
+// reflect value type adequately ensuring to return a pointer if a pointer value
+// was giving to it else returning a value.
+//
+// DeepCopy will never attempt to duplicate the following types, as it will simply
+// return the value it received.
+//
+// 1. Channels
+// 2. Interface
+// 3. unsafe.Pointer
+// 4. Func
+//
+// For structs, we use github.com/jinzhu/copier for copying, so all rules in that library
+// applies here also.
+func DeepCopyValue(baseValue reflect.Value) (interface{}, error) {
+	var refValue, err = deepCopyValue(IndirectValue(baseValue), baseValue.Kind() == reflect.Ptr)
+	if err != nil {
+		return nil, err
+	}
+	return refValue.Interface(), nil
+}
+
+// deepCopyValue copies giving reflect.Value returning a new distinct value containing
+// copy of data, else an error due to failure.
+//
+// DeepCopy will never attempt to duplicate the following types, as it will simply
+// return the value it received.
+//
+// 1. Channels
+// 2. Interface
+// 3. unsafe.Pointer
+// 4. Func
+//
+// For structs, we use github.com/jinzhu/copier for copying, so all rules in that library
+// applies here also.
+func deepCopyValue(baseValue reflect.Value, pointerType bool) (reflect.Value, error) {
+	if !baseValue.IsValid() {
+		return baseValue, nil
+	}
+
+	var valueType = baseValue.Type()
+
+	switch baseValue.Kind() {
+	case reflect.Func:
+		return baseValue, nil
+	case reflect.Interface:
+		return baseValue, nil
+	case reflect.Chan:
+		return baseValue, nil
+	case reflect.UnsafePointer:
+		return baseValue, nil
+	case reflect.Map:
+		var newPtrVal = reflect.MakeMap(baseValue.Type())
+		var newValue = IndirectValue(newPtrVal)
+		if err := copyMap(&newValue, baseValue); err != nil {
+			return reflect.Value{}, err
+		}
+		if pointerType {
+			return newPtrVal, nil
+		}
+		return newValue, nil
+	case reflect.Struct:
+		var newPtrVal = reflect.New(valueType)
+		var newValue = IndirectValue(newPtrVal)
+		if err := copier.Copy(newPtrVal.Interface(), baseValue.Interface()); err != nil {
+			return reflect.Value{}, nerror.Wrap(err, "Failed to copy struct type %T", baseValue.Interface())
+		}
+		if pointerType {
+			return newPtrVal, nil
+		}
+		return newValue, nil
+	case reflect.Array:
+		var newPtrVal = reflect.New(valueType)
+		var newValue = IndirectValue(newPtrVal)
+		var copied = reflect.Copy(newValue, baseValue)
+		if copied != baseValue.Len() {
+			return reflect.Value{}, nerror.New("copied has different length to original")
+		}
+		if pointerType {
+			return newPtrVal, nil
+		}
+		return newValue, nil
+	case reflect.Slice:
+		var newPtrVal = reflect.MakeSlice(valueType, baseValue.Len(), baseValue.Cap())
+		var newValue = IndirectValue(newPtrVal)
+		var copied = reflect.Copy(newValue, baseValue)
+		if copied != baseValue.Len() {
+			return reflect.Value{}, nerror.New("copied has different length to original")
+		}
+		if pointerType {
+			return newPtrVal, nil
+		}
+		return newValue, nil
+	}
+
+	var newPtrValue = reflect.New(valueType)
+	if newPtrValue.CanSet() {
+		return reflect.Value{}, nerror.New("unable to set value for type %q", baseValue.Type())
+	}
+
+	var newValue = IndirectValue(newPtrValue)
+	newValue.Set(baseValue)
+	if pointerType {
+		return newPtrValue, nil
+	}
+	return newValue, nil
+}
+
+func copyMap(dest *reflect.Value, src reflect.Value) error {
 	if dest.Kind() != reflect.Map && src.Kind() != reflect.Map {
 		return nerror.New("unable to copy in between map and another type")
 	}
 
-	var keys = dest.MapKeys()
+	var keys = src.MapKeys()
 	for _, key := range keys {
-		newKey, err := DeepCopy(key)
+		newKey, err := deepCopyValue(IndirectValue(key), key.Kind() == reflect.Ptr)
 		if err != nil {
 			return err
 		}
 
-		newValue, err := DeepCopy(dest.MapIndex(key))
+		var mapValue = src.MapIndex(key)
+		newValue, err := deepCopyValue(IndirectValue(mapValue), mapValue.Kind() == reflect.Ptr)
 		if err != nil {
 			return err
 		}
-		src.SetMapIndex(ValueOf(newKey), ValueOf(newValue))
+		dest.SetMapIndex(newKey, newValue)
 	}
 	return nil
 }
@@ -166,7 +224,7 @@ func CopyStringMap(src map[string]string) map[string]string {
 }
 
 // CopyStringKeyMap returns a new copy of a giving string keyed map.
-func copyStringKeyMap(src map[string]interface{}) (map[string]interface{}, error) {
+func CopyStringKeyMap(src map[string]interface{}) (map[string]interface{}, error) {
 	var dest = make(map[string]interface{}, len(src))
 	for key, value := range src {
 		var myValue, err = DeepCopy(value)
--- nreflect/deepcopy_test.go
+++ nreflect/deepcopy_test.go
@@ -8,10 +8,46 @@ import (
 )
 
 func TestDeepCopy(t *testing.T) {
+	type moma struct {
+		Number int
+	}
+
 	var specs = []struct {
 		Value interface{}
 		Fail  bool
 	}{
+		{
+			Value: map[string]int{"one": 1, "two": 2, "three": 3},
+		},
+		{
+			Value: []string{"one", "two", "three"},
+		},
+		{
+			Value: [3]string{"one", "two", "three"},
+		},
+		{
+			Value: []int{1, 2, 3, 4, 5},
+		},
+		{
+			Value: moma{Number: 20},
+		},
+		{
+			Value: &moma{Number: 20},
+		},
+		{
+			Value: struct {
+				Name string
+			}{
+				Name: "wonder",
+			},
+		},
+		{
+			Value: &(struct {
+				Name string
+			}{
+				Name: "wonder",
+			}),
+		},
 		{
 			Value: "one",
 		},
@@ -22,10 +58,10 @@ func TestDeepCopy(t *testing.T) {
 			Value: 1,
 		},
 		{
-			Value: float64(1),
+			Value: float64(1.6),
 		},
 		{
-			Value: float32(1),
+			Value: float32(11.1),
 		},
 		{
 			Value: true,
@@ -33,18 +69,12 @@ func TestDeepCopy(t *testing.T) {
 		{
 			Value: false,
 		},
-		{
-			Value: []string{"one", "two", "three"},
-		},
-		{
-			Value: []int{1, 2, 3, 4, 5},
-		},
 	}
 
 	for _, spec := range specs {
 		var dest, err = nreflect.DeepCopy(spec.Value)
 		require.NoError(t, err)
 		require.NotNil(t, dest)
-		require.Equal(t, dest, spec.Value)
+		require.Equal(t, spec.Value, dest)
 	}
 }
