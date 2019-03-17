b7ce926e6c79ee3a0392d2390e2a6794c5c0bb19
Alexander Ewetumo <trinoxf@gmail.com>
2019-03-17 23:29:49 +0800

    FIX: Fix unsafe pkg

--- nunsafe/nunsafe.go
+++ nunsafe/nunsafe.go
@@ -10,12 +10,12 @@ import "unsafe"
 // a string, becare not to modify the slice or string, has it will
 // reflect on either.
 func Bytes2String(bc []byte) string {
-	return *(*string)(unsafe.pointer(&bc))
+	return *(*string)(unsafe.Pointer(&bc))
 }
 
 // String2Bytes uses a unsafe.Pointer trick to convert a string into
 // a byte slice, becare not to modify the slice or string, has
 // it will reflect on either.
 func String2Bytes(bc string) []byte {
-	return *(*[]byte)(unsafe.pointer(&bc))
+	return *(*[]byte)(unsafe.Pointer(&bc))
 }
