4575a6660524d1f781139e0f3068374dacdc3f85
Alexander Ewetumo <trinoxf@gmail.com>
2019-03-17 19:30:36 +0800

    UPT: Updated natomics

--- natomic/natomic.go
+++ natomic/natomic.go
@@ -26,7 +26,7 @@ func (f *BoolSwitch) Flip(b bool) {
 }
 
 // State returns the current state of the switch.
-func (f *BoolSwitch) State() bool {
+func (f *BoolSwitch) Read() bool {
 	var state bool
 	f.m.Lock()
 	state = f.fl
@@ -38,20 +38,17 @@ func (f *BoolSwitch) State() bool {
 //
 // Once created, you must not copy a IntSwitch.
 type IntSwitch struct {
-	m  sync.Mutex
 	fl int64
 }
 
 // Flip flips the current value of the giving switch to
 // giving boolean.
 func (f *IntSwitch) Flip(b int64) {
-	f.m.Lock()
 	atomic.StoreInt64(&f.fl, b)
-	f.m.Unlock()
 }
 
 // State returns the current state of the switch.
-func (f *IntSwitch) State() int64 {
+func (f *IntSwitch) Read() int64 {
 	return atomic.LoadInt64(&f.fl)
 }
 
@@ -59,19 +56,52 @@ func (f *IntSwitch) State() int64 {
 //
 // Once created, you must not copy a UintSwitch.
 type UintSwitch struct {
-	m  sync.Mutex
 	fl uint64
 }
 
 // Flip flips the current value of the giving switch to
 // giving boolean.
 func (f *UintSwitch) Flip(b uint64) {
-	f.m.Lock()
 	atomic.StoreUint64(&f.fl, b)
-	f.m.Unlock()
 }
 
 // State returns the current state of the switch.
-func (f *UintSwitch) State() uint64 {
+func (f *UintSwitch) Read() uint64 {
+	return atomic.LoadUint64(&f.fl)
+}
+
+// IntCounter defines a concurrent safe bool switch.
+//
+// Once created, you must not copy a IntCounter.
+type IntCounter struct {
+	fl int64
+}
+
+// Add increments the current value of the giving counter to
+// giving boolean.
+func (f *IntCounter) Add(b int64) {
+	atomic.AddInt64(&f.fl, b)
+}
+
+// State returns the current state of the switch.
+func (f *IntCounter) Read() int64 {
+	return atomic.LoadInt64(&f.fl)
+}
+
+// UintCounter defines a concurrent safe bool switch.
+//
+// Once created, you must not copy a UintCounter.
+type UintCounter struct {
+	fl uint64
+}
+
+// Add increments the current value of the giving counter to
+// giving boolean.
+func (f *UintCounter) Add(b uint64) {
+	atomic.AddUint64(&f.fl, b)
+}
+
+// State returns the current state of the switch.
+func (f *UintCounter) Read() uint64 {
 	return atomic.LoadUint64(&f.fl)
 }
---
+++ natomic/natomic_test.go
@@ -0,0 +1,166 @@
+package natomic
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomicCounter(t *testing.T) {
+	var ws IntCounter
+	var waiter sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		waiter.Add(1)
+		go func() {
+			defer waiter.Done()
+			for i := int64(0); i < 1000; i++ {
+				ws.Add(i)
+			}
+		}()
+	}
+
+	for i := 0; i < 100; i++ {
+		waiter.Add(1)
+		go func() {
+			defer waiter.Done()
+			for i := 0; i < 1000; i++ {
+				ws.Read()
+			}
+		}()
+	}
+
+	for i := 0; i < 1000; i++ {
+		ws.Read()
+	}
+
+	waiter.Wait()
+}
+
+func BenchmarkIntSwitchWrite(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws IntSwitch
+	for i := 0; i < b.N; i++ {
+		ws.Flip(int64(i))
+	}
+}
+
+func BenchmarkAtomicCounter(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws IntCounter
+	var waiter sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		waiter.Add(1)
+		go func() {
+			defer waiter.Done()
+			for i := int64(0); i < 1000; i++ {
+				ws.Add(i)
+			}
+		}()
+	}
+
+	for i := 0; i < 100; i++ {
+		waiter.Add(1)
+		go func() {
+			defer waiter.Done()
+			for i := 0; i < 1000; i++ {
+				ws.Read()
+			}
+		}()
+	}
+
+	for i := 0; i < 1000; i++ {
+		ws.Read()
+	}
+
+	waiter.Wait()
+}
+
+func BenchmarkAtomicInt(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws int64
+	var waiter sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		waiter.Add(1)
+		go func() {
+			defer waiter.Done()
+			for i := 0; i < 1000; i++ {
+				atomic.AddInt64(&ws, 1)
+			}
+		}()
+	}
+
+	for i := 0; i < 100; i++ {
+		waiter.Add(1)
+		go func() {
+			defer waiter.Done()
+			for i := 0; i < 1000; i++ {
+				atomic.LoadInt64(&ws)
+			}
+		}()
+	}
+
+	for i := 0; i < 1000; i++ {
+		atomic.LoadInt64(&ws)
+	}
+
+	waiter.Wait()
+}
+
+func BenchmarkUintSwitchWrite(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws UintSwitch
+	for i := 0; i < b.N; i++ {
+		ws.Flip(uint64(i))
+	}
+}
+
+func BenchmarkBoolSwitchWrite(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws BoolSwitch
+	for i := 0; i < b.N; i++ {
+		ws.Flip(true)
+	}
+}
+
+func BenchmarkIntSwitchRead(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws IntSwitch
+	ws.Flip(int64(1))
+	for i := 0; i < b.N; i++ {
+		ws.Read()
+	}
+}
+
+func BenchmarkUintSwitchRead(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws UintSwitch
+	ws.Flip(uint64(1))
+	for i := 0; i < b.N; i++ {
+		ws.Read()
+	}
+}
+
+func BenchmarkBoolSwitchRead(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var ws BoolSwitch
+	ws.Flip(true)
+	for i := 0; i < b.N; i++ {
+		ws.Read()
+	}
+}
