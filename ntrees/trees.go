8f1244417bddbf42bc53ddba8b20507151aa21ee
Alex Ewetumo <trinoxf@gmail.com>
2020-08-15 18:54:03 +0800

    RMV: Removed ntree

--- ntrees/trees.go
+++
@@ -1,6 +0,0 @@
-// package ntrees provide a virtual tree like DOM structure, for the structure of connecting components
-// able to react to signals (aka events), fast in iteration and navigation.
-package ntrees
-
-//go:generate go run xmlnode_gen.go
-//go:generate go run xmlevent_gen.go
