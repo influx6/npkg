7e8f6ec1ec652fc0eca6ebc922114351470e8f89
Alex Ewetumo <trinoxf@gmail.com>
2020-12-26 01:08:39 +0800

    UPT: Adding NameOf to reflect

--- nreflect/reflection.go
+++ nreflect/reflection.go
@@ -73,6 +73,31 @@ func TypeName(t interface{}) string {
 	return ref.Type().Name()
 }
 
+func NameOf(obj interface{}) string {
+	name, ok := obj.(string)
+	if ok {
+		return name
+	}
+	return nameOfType(reflect.TypeOf(obj))
+}
+
+func nameOfType(t reflect.Type) string {
+	stars := ""
+	if t == nil {
+		return "nil"
+	}
+
+	for t.Kind() == reflect.Ptr {
+		stars += "*"
+		t = t.Elem()
+	}
+
+	if t.Kind() == reflect.Interface {
+		stars = ""
+	}
+	return t.PkgPath() + "/" + stars + t.Name()
+}
+
 // ValidateFunc validates giving function arguments and returns types against TypeValidation
 // functions.
 func ValidateFunc(fn interface{}, argRules, returnRules []TypeValidation) error {
